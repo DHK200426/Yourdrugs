8f6e9b1853be2fb5ddfca106d051e5295b22b96e
DHK200426 <39899853+DHK200426@users.noreply.github.com>
2021-01-29 10:41:26 +0900

    Add files via upload

--- drugshop.py
+++ drugshop.py
@@ -9,19 +9,20 @@ try:
 except ImportError:
     import Image
 
+#이미지 이진화
 img_color = cv.imread("A2.jpg")
 img_eg = cv.Canny(img_color, 100 ,200)
 blur = cv.bilateralFilter(img_color,9,100,100)
-fure_img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
-ret, fure_img_binary = cv.threshold(fure_img_gray, 163, 255, cv.THRESH_BINARY)
 img_gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
 ret, img_binary = cv.threshold(img_gray, 163, 255, cv.THRESH_BINARY)
 img_binary = cv.bitwise_not(img_binary)
+
+#컨투어 그림
 contours,hierarchy = cv.findContours(img_binary, cv.RETR_LIST, cv.CHAIN_APPROX_TC89_KCOS)
+
+#글자 인식
 filename = "{}.png".format(os.getpid()) 
-cv.imwrite(filename, fure_img_binary)
-
-
+cv.imwrite(filename, img_color)
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 # Contour 영역 내에 텍스트 쓰기
@@ -29,6 +30,7 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 
 max_area = 0;
 ci = 0
+
 for i in range(len(contours)):
     cnt = contours[i]
     area = cv.contourArea(cnt)
@@ -65,8 +67,6 @@ def label(image, contour):
 
    minDist = (numpy.inf, None)
 
-
-
    for (i, row) in enumerate(lab):
 
        d = dist.euclidean(row[0], mean)
@@ -99,13 +99,13 @@ elif len(contours) == 3:
 color_text = label(img_color, cnt)
 setLabel(img_color, color_text, cnt)
 
-
-
 epsilon = 0.005 * cv.arcLength(cnt, True)
 approx = cv.approxPolyDP(cnt, epsilon, True)
 
 size = len(approx)
 print(size)
+
+shapet = "error"
 
 cv.line(img_color, tuple(approx[0][0]), tuple(approx[size-1][0]), (0, 255, 0), 3)
 for k in range(size-1):
@@ -114,25 +114,35 @@ for k in range(size-1):
 if cv.isContourConvex(approx):
     if size == 3:
         setLabel(img_color, "triangle", cnt)
+        shapet = 'triangle'
     elif size == 4:
         setLabel(img_color, "rectangle", cnt)
+        shapet = 'rectangle'
     elif size == 5:
         setLabel(img_color, "pentagon", cnt)
+        shapet = 'pentagon'
     elif size == 6:
         setLabel(img_color, "hexagon", cnt)
+        shapet = 'hexagon'
     elif size == 8:
         setLabel(img_color, "octagon", cnt)
+        shapet = 'octagon'
     elif size == 10:
         setLabel(img_color, "decagon", cnt)
+        shapet = 'decagon'
     else:
         setLabel(img_color, "circle", cnt)
+        shapet = 'circle'
 else:
     setLabel(img_color, "circle", cnt)
+    shapet = 'circle'
 
 text = pytesseract.image_to_string(Image.open(filename), lang=None) 
 os.remove(filename) 
 
 print(text)
+print(color_text)
+print(shapet)
 
 rere = cv.resize(img_color,(1280,600))
 cv.imshow("result",rere)
